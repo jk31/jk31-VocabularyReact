e4970fa7c3892d0d8e26b0d4f28eabbea3c64b14
Jarek <jkornowi@mail.upb.de>
2020-03-25 21:37:44 +0100

    started check for input

--- src/components/practice.js
+++ src/components/practice.js
@@ -20,28 +20,48 @@ class Practice extends React.Component {
                     "Word2": "Haus",
                 },
             ],
-            wordToPractice: "Click me to start!"
+            wordToPractice: "Click me to start!",
+            wordToPracticeSolution: "",
+            wordToPracticeUser: "",
         };
     }
 
     random_word = () => {
         var word = this.state.vocabulary[Math.floor(Math.random()*this.state.vocabulary.length)];
-        this.setState({ wordToPractice: word["Word1"] });
+        this.setState({ 
+            wordToPractice: word["Word1"], 
+            wordToPracticeUser: word["Word2"],
+        });
     };
+
+    check_word = () => {
+        if ( this.state.wordToPracticeSolution === this.state.) {
+
+        }
+        this.random_word();
+    }
+
+    handleChange = (event) => {
+        this.setState({wordToPracticeUser: event.target.value});
+    }
 
     render() {
         return (
             <React.Fragment>
                 <main>
-                    <span 
-                    className="practiceWord"
-                    >
+                    <span className="practiceWord">
                         {this.state.wordToPractice}
                     </span>
                     <br></br>
                     <br></br>
-                    <button onClick={this.random_word}>
-                        OK
+
+                    <input type="text" 
+                    value={this.state.wordToPracticeUser} 
+                    onChange={this.handleChange} 
+                    />
+                    <br></br>
+                    <button onClick={this.check_word}>
+                        Check word
                     </button>
                 </main>
             </React.Fragment>
