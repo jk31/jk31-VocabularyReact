730d848ac68462cadce1f8dfc76fdc3778b57d9b
Jarek <jkornowi@mail.upb.de>
2020-03-28 16:40:25 +0100

    configurestore

--- src/index.js
+++ src/index.js
@@ -2,18 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
-import { createStore } from "redux";
 import { Provider } from "react-redux";
-import allReducers from "./reducers/combine"
+import store from "./store/configurestore";
 
 import './index.css';
 import App from './App';
 
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(
-  allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  );
+
 
 ReactDOM.render(
   <div>
